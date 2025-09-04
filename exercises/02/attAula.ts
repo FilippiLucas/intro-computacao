e42108f7be7aa7475112cff0b7664a99a6b7b61d
Lucas Filippi <112347989+FilippiLucas@users.noreply.github.com>
2025-09-04 17:01:59 -0300

    Update attAula.ts

--- exercises/02/attAula.ts
+++ exercises/02/attAula.ts
@@ -2,15 +2,11 @@ import ReadlineSync from "./readline-sync.ts";
 
 let kb = new ReadlineSync();
 
-let valorLiquido:number = 0,
-    percentual:number = 0,
-    valorFinal:number = 0;
+console.log("Informe o valor do produto");
+let valor:number = kb.questionFloat();
 
-console.log("Digite o valor desejado para o produto: ");
-valorLiquido = kb.questionInt();
+console.log("Informe a taxa");
+let taxa:number = kb.questionFloat() / 100;
 
-console.log("Digite o percentual cobrado pelo veiculo: ");
-percentual = kb.questionInt();
-
-valorFinal = valorLiquido + ((valorLiquido * percentual) / (100 - percentual));
-console.log(valorFinal);
+let valorFinal:number = valor / (1 - taxa);
+console.log(valorFinal.toFixed(2));
